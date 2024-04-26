75e0a0f64460b78b66954f412bcf38aa9bdd8ec5
RajaNutakki <rajarajeswari.nutakki@gmail.com>
2024-04-26 11:37:58 +1000

    Initial commit

---
+++ cookieapp/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+def input(request):
+    return render(request,template_name='base.html')
+def add(request):
+    x=int(request.GET['t1'])
+    y=int(request.GET['t2'])
+    z=x+y
+    resp=HttpResponse('values submitted successfully')
+    resp.set_cookie('z',z,max_age=100)
+    return resp
+def display(request):
+    if 'z' in request.COOKIES:
+        sum=request.COOKIES['z']
+        return HttpResponse("addition of two numbers: "+sum)
+    else:
+        return HttpResponse("Please enter values")
+
+
